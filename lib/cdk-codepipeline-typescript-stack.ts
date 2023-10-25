75295ef5d385af95d81446bfc08e432287fc2cbe
Kobus <jacobusbad@gmail.com>
2023-10-24 18:52:19 -0700

    initializing lambda

--- lib/cdk-codepipeline-typescript-stack.ts
+++ lib/cdk-codepipeline-typescript-stack.ts
@@ -2,15 +2,17 @@ import * as cdk from "aws-cdk-lib";
 import {
   CodePipeline,
   CodePipelineSource,
+  ManualApprovalStep,
   ShellStep,
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { PipelineAppStage } from "./stage";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkCodepipelineTypescriptStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new CodePipeline(this, "Pipeline", {
+    const pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "TestPipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub(
@@ -20,5 +22,21 @@ export class CdkCodepipelineTypescriptStack extends cdk.Stack {
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
+
+    const testingStage = pipeline.addStage(
+      new PipelineAppStage(this, "test", {
+        env: { account: "176709227108", region: "us-east-1" },
+      })
+    );
+
+    testingStage.addPost(
+      new ManualApprovalStep("Manual approval before production")
+    );
+
+    const productionStage = pipeline.addStage(
+      new PipelineAppStage(this, "prod", {
+        env: { account: "176709227108", region: "us-east-1" },
+      })
+    );
   }
 }
