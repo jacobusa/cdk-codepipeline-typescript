ec2a3177c672b883906b8b181662bb7ec513698b
Kobus <jacobusbad@gmail.com>
2023-10-24 17:44:34 -0700

    setup cdk

--- bin/cdk-codepipeline-typescript.ts
+++ bin/cdk-codepipeline-typescript.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkCodepipelineTypescriptStack } from '../lib/cdk-codepipeline-typescript-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkCodepipelineTypescriptStack } from "../lib/cdk-codepipeline-typescript-stack";
 
 const app = new cdk.App();
-new CdkCodepipelineTypescriptStack(app, 'CdkCodepipelineTypescriptStack', {
+new CdkCodepipelineTypescriptStack(app, "CdkCodepipelineTypescriptStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,8 @@ new CdkCodepipelineTypescriptStack(app, 'CdkCodepipelineTypescriptStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "176709227108", region: "us-east-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+app.synth();
--- lib/cdk-codepipeline-typescript-stack.ts
+++ lib/cdk-codepipeline-typescript-stack.ts
@@ -1,16 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkCodepipelineTypescriptStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCodepipelineTypescriptQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, "Pipeline", {
+      pipelineName: "Test Pipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "jacobusa/cdk-codepipeline-typescript",
+          "main"
+        ),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
